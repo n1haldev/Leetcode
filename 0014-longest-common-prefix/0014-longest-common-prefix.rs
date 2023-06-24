361857bbf87ffbc70186c5d666affb1cb0f5fb48
Nihal T M <97780641+n1haldev@users.noreply.github.com>
2023-06-24 18:31:44 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (39.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.rs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.rs
@@ -1,27 +1,17 @@
 impl Solution {
-    pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        if strs.is_empty() {
-            return String::new();
-        }
-
-        let mut minlen = strs[0].len();
-        // finding the len of the minimum element to determine the maximum iterations possible
-        for i in 1..strs.len() {
-            if strs[i].len() < minlen {
-                minlen = strs[i].len();
+    pub fn longest_common_prefix(strs: Vec<String>) -> String { 
+        match strs.is_empty() {
+            true => "".to_string(),
+            _ => {
+                strs.iter().skip(1).fold(strs[0].clone(), |acc, x| {
+                    acc
+                        .chars()
+                        .zip(x.chars())
+                        .take_while(|(x, y)| x == y)
+                        .map(|(x, _)| x)
+                        .collect()
+                })
             }
         }
-        
-        let mut prefix = String::new();
-        for i in 0..minlen {
-            let ch = strs[0].chars().nth(i).unwrap();
-            for j in 1..strs.len() {
-                if strs[j].chars().nth(i).unwrap() != ch {
-                    return prefix;
-                }
-            }
-            prefix.push(ch);
-        }
-        prefix
     }
 }
