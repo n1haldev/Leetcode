bb104ef21514acd17adee03f90a3e3541a03b2a4
Nihal T M <97780641+n1haldev@users.noreply.github.com>
2023-06-25 09:59:13 +0530

    Time: 30 ms (7.90%), Space: 2.2 MB (96.82%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut i = 0;
+        let mut j = 1;
+        loop {
+            if i == nums.len() {
+                break;
+            }
+            while j < nums.len() {
+                if nums[i] == nums[j] {
+                    nums.remove(j);
+                } 
+                else {
+                    break;
+                }
+            }
+            i = i + 1;
+            j = j + 1;
+        }
+        nums.len() as i32
+    }
+}
