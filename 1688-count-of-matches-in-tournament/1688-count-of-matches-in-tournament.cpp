3f73082c28ed6e08ef318474659e3bf93c5827f8
Nihal T M <97780641+n1haldev@users.noreply.github.com>
2023-12-05 09:33:29 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.15%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+        if(n&1 == 1) {
+            return (n-1)/2 + numberOfMatches((n-1)/2 + 1);
+        }
+        else {
+            return (n/2) + numberOfMatches(n/2);
+        }
+        return 0;
+    }
+};
